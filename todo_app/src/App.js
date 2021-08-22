6e121247526f25ecbcdb2a73d9df84602379496d
Kumar Dhananjaya <kumar62.shivu@gmail.com>
2021-08-22 21:25:53 +0530

    initial commit

---
+++ todo_app/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState(["Move out ", " hey ", " come on boy "]);
+  const [input, setInput] = useState("");
+  console.log(input);
+
+  const addTodo = (event) => {
+    // this will fire off when we click the button
+    event.preventDefault();
+    console.log("im working");
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello Programmers 🚀</h1>
+      <form>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button type="submit" onClick={addTodo}>
+          {" "}
+          Add Todo{" "}
+        </button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
